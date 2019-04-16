ef0ffc155a2f2dde1328e5b5c35c131b72618e89
NguyenHungHust256 <hungpronguyen256@gmail.com>
2019-04-16 15:20:15 +0700

    chapter 10: updating, showing, and deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :find_user, only: [:show, :edit, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
     @user = User.new
   end
@@ -15,11 +24,50 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".destroy.success"
+      redirect_to users_url
+    else
+      flash[:error] = t ".destroy.unsuccess"
+      redirect_to root_url
+    end
+  end
+
+  def find_user
     @user = User.find_by id: params[:id]
+
     return if @user
     redirect_to root_path
-    flash[:danger] = t(".error")
+    flash[:danger] = t ".error"
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t ".logged_in_user.unsuccess"
+    redirect_to login_url
+  end
+
+  def correct_user
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 
   private
